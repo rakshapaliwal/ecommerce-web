73aec956e21ed94fc6c9590a7c7d1350e4637754
MonikaPrajapat <monuprj@gmail.com>
2024-02-16 18:08:27 +0530

    add contact file by raksha

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,11 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/registration/Login";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
+import Contact from "./components/Contact/Contact";
+
 import SignIn from "./components/registration/SignIn";
 import Footer from "./components/Footer/Footer";
+
 
 function App() {
   const [count, setCount] = useState(0);
@@ -17,6 +20,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signin" element={<SignIn />} />
+          <Route path="/contact" element={<Contact />} />
+
         </Routes>
         <Footer />
       </Router>
